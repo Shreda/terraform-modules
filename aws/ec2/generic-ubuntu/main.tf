d3293a75f98632c31214869fa838f4371162e0ed
Daniel Moore <danielmoore.info@gmail.com>
2020-01-16 13:43:56 +1100

    spellilng

--- aws/ec2/generic-ubuntu/main.tf
+++ aws/ec2/generic-ubuntu/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2instance" {
   ami             = "ami-00a54827eb7ffcd3c"
   instance_type   = var.instance_type
   key_name        = aws_key_pair.ec2key.key_name
-  vpc_security_groups_ids = var.security_groups
+  vpc_security_group_ids = var.security_groups
 
   tags = {
     Name = var.name 
