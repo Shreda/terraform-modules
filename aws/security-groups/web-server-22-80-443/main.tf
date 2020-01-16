64bd571547d74cd1635f9c5d887bd50f25bbd944
Daniel Moore <danielmoore.info@gmail.com>
2020-01-16 11:20:29 +1100

    Added web server sg

---
+++ aws/security-groups/web-server-22-80-443/main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "webserversg" {
+  name = "webserversg"
+  vpc_id = var.vpc_id
+  description = "Allows port 80 and 443"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
