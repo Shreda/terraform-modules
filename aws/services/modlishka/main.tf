d2605f0a177a1968b34f6b21b260c038b8852ab4
Daniel Moore <danielmoore.info@gmail.com>
2020-01-20 13:17:22 +1100

    added modlishka service

---
+++ aws/services/modlishka/main.tf
@@ -0,0 +1,20 @@
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+module "sg" {
+  source = "github.com/danielmoore-info/terraform-modules//aws/security-groups/web-server-22-80-443"
+  vpc_id = aws_default_vpc.default.id
+}
+module "server" {
+  source          = "github.com/danielmoore-info/terraform-modules//aws/ec2/generic-ubuntu"
+  public_key      = var.public_key
+  security_groups = [
+    module.sg.sg_id 
+  ]
+  name            = var.name
+  instance_type   = var.instance_type
+}
+
---
+++ aws/services/modlishka/variables.tf
@@ -0,0 +1,22 @@
+variable "instance_type" {
+  type        = "string"
+  description = "size of the ec2 intance"
+  default     = "t2.small"
+}
+
+variable "public_key" {
+  type        = "string"
+  description = "Public key to install onto the instance"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Metadata name for the intance"
+  default     = "Generic Ubuntu"
+}
+
+variable "instance_type" {
+  type        = "string"
+  description = "size of the ec2 intance"
+  default     = "t2.small"
+}
