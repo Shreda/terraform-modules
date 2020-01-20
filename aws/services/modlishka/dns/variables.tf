113ee044efb23f654da1cf82e7ff8e7e2c76aa72
Daniel Moore <danielmoore.info@gmail.com>
2020-01-20 14:28:31 +1100

    added variables to modlishka DNS config

---
+++ aws/services/modlishka/dns/variables.tf
@@ -0,0 +1,9 @@
+variable  "domain" {
+  type = "string"
+  description = "domain to use for setup"
+}
+
+variable "public_ip" {
+  type = "string"
+  description = "public IP of the mailcow server"
+}
